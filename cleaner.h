86209e6b8bed9373c3e51f22ba75ef54e7210cad
Edward FANG <fangyd1997@outlook.com>
2017-08-15 09:36:13 +0800

    check_privilege; file_fingerprint

--- cleaner.h
+++ cleaner.h
@@ -9,15 +9,26 @@
 #include <sys/stat.h>
 #include <string.h>
 #include <stdarg.h>
+#include <errno.h>
 
 #endif
 
-#define MAX_MSG_LEN 1000
+#define MIN_BLOCK_COMPARE_SIZE 150 
+#define BLOCK_SIZE 50
 
 extern int verbose_mod;
 extern int debug_mod;
 extern char* rootpath;
 extern int rootpathlen;
+
+typedef struct{
+    char* filename;
+    off_t filesize;
+    mode_t st_mode;
+    unsigned char * md5;
+} file_fingerprint;
+
+file_fingerprint* tree_root;
 
 int main(int argc, char* argv[]);
 
@@ -31,12 +42,7 @@ extern int compare(const char * filepath);
 
 unsigned char* getMD5(const char *filepath);
 
-// filefingerprint
-// typedef struct{
-//     char* filename;
-//     off_t st_size;
-//     mode_t st_mode;
-// } file_fingerprint;
+int  check_privilege(const char* filepath);
 
 // struct filenode {
 //     //struct
--- file_compare.c
+++ file_compare.c
@@ -1,9 +1,14 @@
 #include "cleaner.h"
 #include <openssl/md5.h>
-#include <fcntl.h>
+#include <fcntl.h> //file control options
+
 
 int compare(const char *filepath)
 {
+    if(check_privilege(filepath)){
+        verbose_msg("Ignore file: filename due to privilege limitation: %s", strerror(errno));
+        //return -1;
+    }
     struct stat buffer;
     int status;
     status = lstat(filepath, &buffer);
@@ -14,6 +19,31 @@ int compare(const char *filepath)
     return 0;
 }
 
+// ignore the files that can not be read; test privilege
+int check_privilege(const char* filepath){
+    FILE* fd;
+    if((fd = fopen(filepath, "rb"))==NULL){
+        return 1;
+    }else{
+        fclose(fd);
+        return 0;
+    }
+}
+
+
+// the third comparision
+int compare_file_blocks(file_fingerprint* file1,file_fingerprint file2 ){
+    off_t file_size = file1->filesize;
+    if(file_size>MIN_BLOCK_COMPARE_SIZE){
+        return 0;
+    }else{
+        return 0;
+    }
+
+}
+
+
+// the final comparision
 unsigned char *getMD5(const char *filepath)
 {
     int n;
@@ -24,8 +54,8 @@ unsigned char *getMD5(const char *filepath)
 
     MD5_Init(&c);
     debug_msg("MD5_Init");
-    int fd;
-    fd = open(filepath, O_RDONLY);
+    int fd; // FILE* fd;
+    fd = open(filepath, O_RDONLY); // or fd = fopen(filepath, "rb"); 
     bytes = read(fd, buf, 512);
     debug_msg("MD5_read");
     while (bytes > 0)
